9621c73eff8aa0b2f3ecabcac61f78854e37e9bb
Breno Andrade Costa <breno.ac@gmail.com>
2024-07-08 21:19:08 -0300

    Limpeza de códigos duplicados

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,41 +1,9 @@
-// import styled from 'styled-components';
-// import { cores } from '../../styles';
-
-// export const Container = styled.footer`
-//   background-color: ${cores.rosa_claro_tag};
-//   width: 1366px;
-//   height: 298px;
-// `;
-
-// export const Logo = styled.div`
-//   padding-left: 621px;
-//   padding-top: 40px;
-//   width: 100%;
-// `;
-// export const Links = styled.div`
-//   width: 24px;
-//   height: 24px;
-//   display: flex;
-//   gap: 8px;
-//   margin-left: 639px;
-//   margin-top: 32.5px;
-// `;
-
-// export const Texto = styled.p`
-//   font-size: 10px;
-//   font-weight: 400;
-//   color: ${cores.rosa};
-//   line-height: 11.72px;
-//   text-align: center;
-//   margin-top: 80px;
-// `;
-
 import styled from 'styled-components';
 import { cores } from '../../styles';
 
 export const Container = styled.footer`
   background-color: ${cores.rosa_claro_tag};
-  width: 100%; /* Ajustado para 100% */
+  width: 100%;
   height: 298px;
   margin-top: -240px; /* Subtraído para ajustar o espaço */
 `;
--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,18 +1,3 @@
-// import styled from 'styled-components';
-
-// export const Container = styled.div`
-//   padding-top: 80px;
-//   padding-bottom: 120px;
-//   height: 100%;
-// `;
-
-// export const List = styled.ul`
-//   display: grid;
-//   grid-template-columns: 1fr 1fr;
-//   column-gap: 80px;
-//   width: 1024px;
-// `;
-
 import styled from 'styled-components';
 
 export const Container = styled.div`
