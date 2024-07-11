d7aaabb7337b4e8ef0e18afc3cdf7a791bebd996
Breno Andrade Costa <breno.ac@gmail.com>
2024-07-11 00:56:48 -0300

    Segunda correção de responsividade

--- src/components/Hero2/index.tsx
+++ src/components/Hero2/index.tsx
@@ -1,5 +1,4 @@
 import { Imagem, LogoContainer, Apresenta, Titulo } from './styles';
-
 import heroImg from '../../assets/images/fundo.png';
 import logo from '../../assets/images/logo.png';
 import apresentacao from '../../assets/images/apresentacao.png';
@@ -9,13 +8,16 @@ const Hero2 = () => (
     <LogoContainer>
       <img src={logo} alt="EFOOD" />
     </LogoContainer>
-
     <Titulo>
       <p>Restaurantes</p>
       <p>0 produto(s) no carrinho</p>
     </Titulo>
     <Apresenta>
       <img src={apresentacao} alt="Apresentação" />
+      <div className="text-wrapper">
+        <div className="text-italiana">italiana</div>
+        <div className="text-dolceVita">La Dolce Vita Trattoria</div>
+      </div>
     </Apresenta>
   </Imagem>
 );
--- src/components/Hero2/styles.ts
+++ src/components/Hero2/styles.ts
@@ -30,26 +30,57 @@ export const Titulo = styled.h2`
   text-align: center;
   display: flex;
   justify-content: center;
-  align-items: center;
   position: absolute;
-  gap: 659px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  gap: 710px;
   width: 100%;
-  margin: 0;
-  z-index: 1;
 `;
 
 export const Apresenta = styled.div`
   position: absolute;
   top: calc(100%);
-  left: 50%;
-  transform: translateX(-50%);
   display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
   height: 280px;
   z-index: 0;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    min-width: 100vw;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .text-wrapper {
+    position: absolute;
+    top: 25px;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+
+  .text-italiana {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 100;
+    font-size: 32px;
+    line-height: 37.5px;
+    width: 101px;
+    height: 33.25px;
+    color: ${cores.branca};
+  }
+
+  .text-dolceVita {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 900;
+    font-size: 32px;
+    line-height: 37.5px;
+    padding-top: 156.5px;
+    text-align: left;
+    width: 676px;
+    height: 33.25px;
+    color: ${cores.branca};
+  }
 `;
